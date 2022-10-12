dec1a7bbefc394477012e65653253f8865ac75b2
thaithuytb <72178224+thaithuytb@users.noreply.github.com>
2022-10-12 09:03:47 +0700

    fix package.json file and config databse to deploy

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -3,14 +3,14 @@ import { DataSourceOptions } from 'typeorm';
 export default {
   type: 'mysql',
   host: process.env.MYSQL_HOST,
-  port: parseInt(process.env.MYSQL_PORT as string),
+  port: 3306,
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
-  entities: ['src/entities/**.entity.ts'],
-  migrations: ['src/migrations/*.ts'],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  migrations: [__dirname + '/../migrations'],
   cli: {
     migrationsDir: 'src/migrations',
   },
-  synchronize: true,
+  synchronize: false,
 } as DataSourceOptions;
